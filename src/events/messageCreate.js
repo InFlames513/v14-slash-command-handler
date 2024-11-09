d9b842d7cfa6b76a6663a6d08f5c0fd256655f59
İsmetcan Yalçın <85372327+InFlames513@users.noreply.github.com>
2024-11-10 02:59:52 +0300

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,7 +1,6 @@
 module.exports = {
-	name: 'messageCreate',
-	execute: async(message) => {
+ name: 'messageCreate',
+ execute: async(message) => {
   let client = message.client;
-  if (message.author.bot) return;
-  if (message.channel.type === 'dm') return;
-  }};
+  if (message.author.bot || message.channel.type === 'dm') return;
+}};
